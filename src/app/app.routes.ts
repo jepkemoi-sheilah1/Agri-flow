ffe32ab0f4586a39484e8a9131604dfd23dcb027
jepkemoi-sheilah1 <jepkemoi-sheilah1@users.noreply.github.com>
2026-05-28 10:34:24 +0300

    feat: add login page with Angular Material and Tailwind styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,13 @@
 import { Routes } from '@angular/router';
 
-
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login').then(m => m.LoginComponent)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+];
---
+++ src/app/features/auth/login/login.ts
@@ -0,0 +1,86 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router,RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/auth/auth.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule,MatIconModule , RouterLink, MatProgressSpinnerModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class LoginComponent implements OnInit {
+
+  // 1. Injected services
+  private router = inject(Router);
+  private authService = inject(AuthService);
+  private formBuilder = inject(FormBuilder);
+
+  // 2. Properties
+  loginForm!: FormGroup;
+  isLoading = false;
+  errorMessage = '';
+  showPassword = false;
+
+  // 3. Lifecycle hooks
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  // 4. Methods
+  onSubmit(): void {
+    
+  // 1. Return early if form is invalid
+  if (this.loginForm.invalid) {
+    return;
+  }
+
+  // 2. Start loading
+  this.isLoading = true;
+  this.errorMessage = '';
+
+  // 3. Get email and password from form
+  const { email, password } = this.loginForm.value;
+
+  // 4. Call login service
+  this.authService.login({ email, password}).subscribe({
+    next: (response) => {
+
+      // 5. Check roles and navigate
+      const roles: string[] = response.data.user.roles || [];
+
+      if (roles.includes('SUPER_ADMIN')) {
+        this.router.navigate(['/super-admin']);
+      } else if (roles.includes('ADMIN')) {
+        this.router.navigate(['/admin']);
+      } else if (roles.includes('SELLER')) {
+        this.router.navigate(['/seller']);
+      } else {
+        this.router.navigate(['/buyer']);
+      }
+
+      // 7. Stop loading
+      this.isLoading = false;
+    },
+
+    error: (error) => {
+      // 6. Set error message
+      this.errorMessage =
+        error?.error?.message || 'Login failed. Please try again.';
+
+      // 7. Stop loading
+      this.isLoading = false;
+    }
+  });
+}
+  }
+
